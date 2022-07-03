ca91a998792950dbe569e4a20bdc7131ab35c866
wuddy233 <108476458+wuddy233@users.noreply.github.com>
2022-07-03 10:47:20 +0800

    test-site
    html, css,js Demo

---
+++ test-site/scripts/main.js
@@ -0,0 +1,35 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src', 'images/1637643662464.jpeg');
+    } else {
+      myImage.setAttribute('src', 'images/1.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = '七龙珠酷毙了，' + myName;
+    }
+}
+
+//初始化网页的代码，如果之间用户访问网页设置过name，那就用上次的名字
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
